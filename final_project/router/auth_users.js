d97e55f217facdda3a2f30b11d295f23f7c95436
davegosbee <daveownsthis@hotmail.com>
2022-12-20 20:31:24 -0500

    initial checkin

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,20 +3,37 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+    {
+    username: 'administrator',
+    password: 'password'
+    },
+];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    let filtered_users = users.filter((user) => user.username === username);
+    return filtered_users.length > 0;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let filtered_users = users.filter((user) => user.username === username && user.password === password);
+    return filtered_users.length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const user = req.body.user;
+    if (!user) {
+        return res.status(404).json({message: "Body Empty"});
+    }
+    let accessToken = jwt.sign({
+        data: user
+      }, "fingerprint_customer", { expiresIn: 60 * 60 });
+      req.session.authorization = {
+        accessToken
+    }
+    return res.status(200).json({message: "User successfully logged in"});
+   
 });
 
 // Add a book review
