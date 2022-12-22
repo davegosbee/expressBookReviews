99a890a9f1ce136b88fc72a177fdb86ff7169e08
davegosbee <daveownsthis@hotmail.com>
2022-12-22 11:10:56 -0500

    allow register and login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -23,17 +23,26 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //only registered users can login
 regd_users.post("/login", (req,res) => {
     const user = req.body.user;
+    
     if (!user) {
         return res.status(404).json({message: "Body Empty"});
     }
-    let accessToken = jwt.sign({
-        data: user
-      }, "fingerprint_customer", { expiresIn: 60 * 60 });
-      req.session.authorization = {
-        accessToken
+    const {username, password} = req.body.user;
+    if(authenticatedUser(username, password))
+    {
+        let accessToken = jwt.sign({
+            data: user
+        }, "fingerprint_customer", { expiresIn: 60 * 60 });
+        
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).json({message: "User successfully logged in"});
     }
-    return res.status(200).json({message: "User successfully logged in"});
-   
+    else
+    {
+        return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
 });
 
 // Add a book review
